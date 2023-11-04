6d6bae16ee8cc24c0baa927361934a2b7cb501e4
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2023-11-04 22:17:03 +0100

    added moving background

--- game5/script.js
+++ game5/script.js
@@ -8,12 +8,27 @@ const collisionCtx = collisionCanvas.getContext('2d');
 collisionCanvas.width = window.innerWidth; 
 collisionCanvas.height = window.innerHeight;
 
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = './background1/layer-1.png';
+
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = './background1/layer-2.png';
+
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = './background1/layer-3.png';
+
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = './background1/layer-4.png';
+
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = './background1/layer-5.png';
+
 let score = 0;
 let gameOver = false; 
 ctx.font = '50px Impact'; 
 
-let timeToNextRaven = 0;
-let ravenInterval = 300;
+let timeToNextRaven = 10;
+let ravenInterval = 600;
 let lastTime = 0;
 
 let ravens = []; 
@@ -21,13 +36,13 @@ class Raven {
     constructor(){
         this.spriteWidth = 271; 
         this.spriteHeight = 175; 
-        this.sizeModifier = Math.random()*0.6 + 0.4;
+        this.sizeModifier = Math.random()*1.2 + 0.5;
         this.width = this.spriteWidth*this.sizeModifier;
         this.height = this.spriteHeight*this.sizeModifier; 
         this.x = canvas.width; 
         this.y = Math.random()*(canvas.height -this.height); 
-        this.directionX = Math.random()*7 +3;
-        this.directionY = Math.random()*7 -2.5; 
+        this.directionX = Math.random()*5 +3;
+        this.directionY = Math.random()*5 -2.5; 
         this.markedForDeletion = false; 
         this.image = new Image(); 
         this.image.src = './images/raven.png';
@@ -98,6 +113,7 @@ class Explosion {
 }
 
 let particles = []; 
+let gameSpeed = 5; 
 class Particle {
     constructor(x,y,size,color){
         this.size = size; 
@@ -125,6 +141,44 @@ class Particle {
         ctx.fill();  
     }
 }
+
+class Layer {
+    constructor(image, speedmModifier) {
+        this.x = 0; //set x property on object created rn
+        this.y = 0;
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.image = image; //parameter passed will be attribute
+        this.speedmModifier = speedmModifier; //how fast
+        this.speed = gameSpeed * this.speedmModifier; //each layer has different speed linked to general
+    }
+    update() {
+        this.speed = gameSpeed * this.speedmModifier;
+
+        if (this.x <= -this.width) {
+            this.x = 0;
+        }
+        this.x = this.x - this.speed;
+
+        //this.x = gameFrame*this.speed % this.width; 
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+
+    }
+}
+
+const layer1 = new Layer(backgroundLayer1, 1.5);
+const layer2 = new Layer(backgroundLayer2, 0.5);
+const layer3 = new Layer(backgroundLayer3, 0.8);
+const layer4 = new Layer(backgroundLayer4, 0.6); //creates new object of class, calling its constructor 
+//as it expects 2 parameters passed 
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
 
 function drawScore(){
     ctx.fillStyle = 'black'; 
@@ -166,6 +220,11 @@ window.addEventListener('click', function(e){
 
 function animate(timestamp){
     ctx.clearRect(0,0,canvas.width, canvas.height);
+    gameObjects.forEach(layer => {
+        layer.update(); 
+        layer.draw(); 
+    }); 
+
     collisionCtx.clearRect(0,0,canvas.width, canvas.height); 
     let deltatime = timestamp - lastTime;
     lastTime = timestamp; 
