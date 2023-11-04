1c96c27f0a0e5e92fecccbf3b4e226f3f5d577bd
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2023-11-04 15:47:27 +0100

    adding sound effect to collision detection

--- game4/script.js
+++ game4/script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas1'); 
 const ctx = canvas.getContext('2d'); 
-canvas.width = 500;
+canvas.width = 600;
 canvas.height = 700; 
 const explosions = []; 
 
@@ -14,39 +14,47 @@ class Explosion {
         this.spriteHeight = 179; 
         this.width = this.spriteWidth*0.7; 
         this.height =  this.spriteHeight*0.7; 
-        this.x = x - this.width/2;
-        this.y = y - this.height/2; 
+        this.x = x;
+        this.y = y; 
         this.image = new Image(); 
         this.image.src = './boom.png'; 
         this.frame = 0; 
         this.timer = 0; 
+        this.angle = Math.random() * 6.2; 
+        this.sound = new Audio(); 
+        this.sound.src = 'boom.wav'; 
     }
     update(){
+        if(this.frame === 0) this.sound.play(); 
         this.timer++; 
         if(this.timer % 10 === 0){
             this.frame++;
         }
     }
     draw(){
-        ctx.drawImage(this.image, this.frame*this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
-        console.log("image drawn"); 
+        ctx.save(); 
+        ctx.translate(this.x, this.y); 
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.frame*this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, 0 - this.width/2, 0-this.height/2, this.width, this.height);
+        ctx.restore(); 
     }
 }
 
 window.addEventListener('click', function(e){
-    console.log("hello");
-    //problem: have to take into account also canvas positioning for "repositioning"
-    //-25 so will create rectangle from "center" (50/2 for both x and y)
+    createAnimation(e); 
+})
+
+/*window.addEventListener('mousemove', function(e){
+    createAnimation(e); 
+})*/
+
+function createAnimation(e){
     let positionX = e.x - canvasPosition.left; 
     let positionY = e.y - canvasPosition.top ; 
     
     explosions.push(new Explosion(positionX, positionY)); 
 
-    console.log(explosions);
-
-})
-
-
+}
 
 function animate(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
