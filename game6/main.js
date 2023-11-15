2d8aa0443257c81e3f4e05c6d92541ab216345b6
giuliaprosio <82225149+giuliaprosio@users.noreply.github.com>
2023-11-15 23:32:43 +0100

    Added ravens in collision detection game

--- game6/main.js
+++ game6/main.js
@@ -1,8 +1,9 @@
 import {Player} from './player.js'; 
 import { InputHandler } from './inputHandler.js';
 import { Layer } from './layers.js';
+import { Raven } from './ravens.js';
 
-let gameSpeed = 5;
+let lastTime = 0; 
 
 window.addEventListener('load', function(){
     const canvas = this.document.getElementById('canvas1'); 
@@ -23,8 +24,13 @@ window.addEventListener('load', function(){
             this.back3 = new Layer(document.getElementById('background-3')); 
             this.back4 = new Layer(document.getElementById('background-4')); 
             this.back5 = new Layer(document.getElementById('background-5')); 
+
+            this.ravens = []; 
+            this.ravensTimer = 0;
+            this.ravensInterval = 1000; 
+            
         }
-        update(){
+        update(deltatime){
             // this.input.keys = [];
        
             this.player.update(this.input.keys);
@@ -33,6 +39,16 @@ window.addEventListener('load', function(){
             this.back3.update(this.player.x); 
             this.back2.update(this.player.x);
             this.back1.update(this.player.x);
+            if(this.ravensTimer > this.ravensInterval){
+                this.addRaven(); 
+                this.ravensTimer = 0; 
+            }else{ 
+                this.ravensTimer += deltatime; 
+            }
+
+            this.ravens.forEach(obj => {
+                obj.update(deltatime); 
+            });
             
         }
         draw(context){
@@ -43,6 +59,17 @@ window.addEventListener('load', function(){
             this.back4.draw(context);
             this.back5.draw(context);
             this.player.draw(context); 
+            this.ravens.forEach( raven => {
+                raven.draw(context); 
+            });
+            
+        }
+
+        addRaven(){
+
+            this.ravens.push(new Raven(this)); 
+            console.log(this.ravens);
+            
         }
     }
 
@@ -50,13 +77,16 @@ window.addEventListener('load', function(){
     console.log(game); 
    
 
-    function animate(){
+    function animate(timestamp){
         ctx.clearRect(0,0,canvas.width, canvas.height);
+        let deltatime = timestamp - lastTime; 
+        lastTime = timestamp; 
         game.draw(ctx); 
-        game.update();
+        game.update(deltatime);
         requestAnimationFrame(animate); 
     }
-    animate(); 
+
+    animate(0); 
 
 
 });
---
+++ game6/ravens.js
@@ -0,0 +1,60 @@
+export class Raven {
+    constructor(game){
+        this.frameX = 0; 
+        this.frameY = 0; 
+        this.frameInterval = 8; 
+        this.frameTimer = 0;
+
+        this.game = game;
+
+        this.spriteWidth = 271; 
+        this.spriteHeight = 175; 
+        this.sizeModifier = Math.random()*1.2 + 0.5;
+        this.width = this.spriteWidth*this.sizeModifier;
+        this.height = this.spriteHeight*this.sizeModifier; 
+
+        this.x = this.game.width;
+        this.y = this.y = Math.random()*(this.game.height -this.height); ;
+        this.speedX = 2; 
+        this.speedY = 0;
+        this.maxFrame = 5; 
+
+        this.directionX = Math.random()*5 +3;
+        this.directionY = Math.random()*5 -2.5; 
+
+        this.image = document.getElementById('raven'); 
+
+        this.timeSinceFlap = 0;
+    }
+
+    update(deltatime){
+        if(this.y <= 0) console.log(this.y)
+        if(this.y < 0 || this.y > this.game.height -this.height){
+            this.directionY = this.directionY * -1;
+        }
+        this.x -= this.directionX;
+        this.y += this.directionY;
+       
+        if(this.x < 0 -this.width) this.markedForDeletion = true;
+        this.timeSinceFlap += deltatime; 
+       
+        if(this.frameTimer >= this.frameInterval){
+            this.frameTimer = 0; 
+            if(this.frameX <= 4 ) this.frameX++;
+            else this.frameX = 0;
+        }else{
+            this.frameTimer ++; 
+        }
+        this.timeSinceFlap = 0;
+        //particles.push(new Particle(this.x, this.y, this.width, this.color)); 
+        
+       // if(this.x < 0 - this.width) gameOver = true; 
+    }
+
+    draw(context){
+
+        context.strokeRect(this.x, this.y, this.width, this.height); 
+        context.drawImage(this.image, this.frameX*this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height); 
+
+    }
+}
